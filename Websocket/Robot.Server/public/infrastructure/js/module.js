d63893c19a9109d5cc64af7297d52b467c43c9c2
Soulskater <gmeszaros007@gmail.com>
2015-07-27 17:59:08 +0200

    UI update

--- Websocket/Robot.Server/public/infrastructure/js/module.js
+++ Websocket/Robot.Server/public/infrastructure/js/module.js
@@ -1,4 +1,13 @@
 angular.module("RobotControl", ["ngMaterial", "ngRoute"])
+    .config(function ($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .dark(false);
+    })
+    .config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue')
+            .accentPalette('orange');
+    })
     .run(function (protoBufService) {
         protoBufService.loadProtos();
     });
